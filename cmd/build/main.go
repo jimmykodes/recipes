13d5b8964549663b8e058ed560beafb54bd62f50
jimmykodes <jimmy@khanacademy.org>
2023-12-16 15:49:46 -0800

    feat: use Sections for ingredients and procedures

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -23,13 +23,15 @@ var (
 )
 
 type Recipe struct {
-	Title       string   `yaml:"title"`
-	Tags        []string `yaml:"tags"`
-	Ingredients []struct {
-		Name  string   `yaml:"name"`
-		Items []string `yaml:"items"`
-	} `yaml:"ingredients"`
-	Procedure []string `yaml:"procedure"`
+	Title       string    `yaml:"title"`
+	Tags        []string  `yaml:"tags"`
+	Ingredients []Section `yaml:"ingredients"`
+	Procedure   []Section `yaml:"procedure"`
+}
+
+type Section struct {
+	Name  string   `yaml:"name"`
+	Items []string `yaml:"items"`
 }
 
 type Link struct {
